fdfbaa1d70c2a25b54b669044854e478bb1191cb
vlad <0309vlad2009@gmail.com>
2026-04-21 15:33:48 +0300

    new migrations

---
+++ laptop_server/Migrations/20260420225635_InternetDocGenerationInfo.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaptopServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InternetDocGenerationInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "Orders",
+                newName: "CustomerInfo_PhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Orders",
+                newName: "CustomerInfo_Email");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentId",
+                table: "Orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryCityRef",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerInfo_FirstName",
+                table: "Orders",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerInfo_LastName",
+                table: "Orders",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerInfo_MiddleName",
+                table: "Orders",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NpContactRecipientRef",
+                table: "Orders",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NpRecipientRef",
+                table: "Orders",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrackingDocNum",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrackingDocRef",
+                table: "Orders",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VolumeGeneral",
+                table: "OrderItems",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Weight",
+                table: "OrderItems",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VolumeGeneral",
+                table: "Laptops",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Weight",
+                table: "Laptops",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerInfo_FirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerInfo_LastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerInfo_MiddleName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "NpContactRecipientRef",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "NpRecipientRef",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TrackingDocNum",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TrackingDocRef",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "VolumeGeneral",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "VolumeGeneral",
+                table: "Laptops");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Laptops");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerInfo_PhoneNumber",
+                table: "Orders",
+                newName: "PhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerInfo_Email",
+                table: "Orders",
+                newName: "Email");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryCityRef",
+                table: "Orders",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ laptop_server/Migrations/20260421122839_BiggerRefForNp.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaptopServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class BiggerRefForNp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingDocRef",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NpRecipientRef",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NpContactRecipientRef",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingDocRef",
+                table: "Orders",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NpRecipientRef",
+                table: "Orders",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NpContactRecipientRef",
+                table: "Orders",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
